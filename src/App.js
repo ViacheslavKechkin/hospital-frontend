798002e68b1eb27b059bda779652d76baf9ec7d4
vykechkin <vykechkin.exceedteam@gmail.com>
2022-03-23 11:44:17 +0300

    create validation for registration input fields

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
 
+import React, { useContext, useEffect } from "react";
+import { Routes, Route } from 'react-router-dom';
+import axios from "axios";
+import MyContext from './context';
+import Mysnackbar from "./components/MUI/Mysnackbar/Mysnackbar";
 import Registration from './components/Registration/Registration';
 import logoHospital from './img/logohospital.png'
 import './App.scss';
 
 const App = () => {
-  const flagHeader = 'registration';
+  const { flagHeader, mySnackBar, setMySnackBar, users, setUsers } = useContext(MyContext);
+  const { open } = mySnackBar;
+
+  const handleCloseBar = () => {
+    setMySnackBar({ open: false });
+  };
+
+  useEffect(() => {
+    axios.get('http://localhost:9000/allUsers').then(res => {
+      setUsers(res.data.data);
+    });
+  }, [setUsers])
+
+  console.log(users);
 
   return (
     <div className="App">
       <header>
-        <img src={logoHospital} alt="LogoHospital" />
+        <img src={logoHospital} alt="LogoHospital" className='main-logo' />
         {flagHeader === 'registration' && (
           <div className='headerTitle'>Зарегистрироваться в системе</div>
         )}
       </header>
-      <Registration />
+      <Mysnackbar
+        open={open}
+        handleCloseBar={handleCloseBar}
+      />
+      <Routes>
+        <Route path='/' element={'#'} />
+        <Route path='/registration' element={
+          <Registration />
+        } />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
@@ -7,18 +7,32 @@ import App from './App';
 import './index.scss';
 
 const Main = () => {
+  //the flag will change on the login page when you click on register 
+  //change to authorization when will the login page be ready
+  const [flagHeader, setFlagHeader] = useState('registration');
+  const [users, setUsers] = useState([]);
+  const [mySnackBar, setMySnackBar] = useState({ open: false });
+  const [messageSnackBar, setMessageSnackBar] = useState("");
+
   return (
     <React.StrictMode>
-      <MyContext.Provider value={'#'}>
+      <MyContext.Provider value={{
+        users,
+        setUsers,
+        setMySnackBar,
+        flagHeader,
+        setFlagHeader,
+        mySnackBar,
+        messageSnackBar,
+        setMessageSnackBar
+      }}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
       </MyContext.Provider>
     </React.StrictMode>
   )
-
 }
-
 
 ReactDOM.render(
   <Main />,
